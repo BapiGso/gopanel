0963e91960bd78104c15724ac14ad130e16c2c38
nil <nil>
2026-01-17 13:44:57 +0800

    monitor.template

--- core/x509keyPair.go
+++ core/x509keyPair.go
@@ -6,59 +6,32 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"log"
 	"math/big"
 	"time"
 )
 
 var certPEM, keyPEM = func() ([]byte, []byte) {
-	// 1. 生成私钥（2048位 RSA）
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		log.Fatalf("无法生成私钥: %v", err)
-	}
-	serialNumber := big.NewInt(0)
-	serialNumber.SetBytes([]byte{0x46, 0x78, 0x58, 0xB7})
-	template := x509.Certificate{
-		SerialNumber: serialNumber,
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	tmpl := x509.Certificate{
+		SerialNumber: big.NewInt(1182324919),
 		Subject: pkix.Name{
-			Country:            []string{"JP"},
 			Province:           []string{"Lotus Land Story"},
-			Locality:           []string{"ShangHai"},
-			Organization:       []string{"Arisu"},
-			OrganizationalUnit: []string{"Gengakudan"},
 			CommonName:         "ShangHai",
-		},
-		NotBefore:             time.Date(2024, 5, 10, 8, 38, 32, 0, time.UTC),
-		NotAfter:              time.Date(2034, 5, 8, 8, 38, 32, 0, time.UTC),
+			Organization:       []string{"Arisu"},
+			OrganizationalUnit: []string{"Gengakudan"}},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1996, 11, 3),
+		IsCA:                  true,
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 		BasicConstraintsValid: true,
-		IsCA:                  true, // 这是一个 CA 证书
 	}
-	// 3. 创建自签名证书
-	certDER, err := x509.CreateCertificate(
-		rand.Reader,
-		&template,
-		&template, // 自签名：签发者是自己
-		&privateKey.PublicKey,
-		privateKey, // 用自己的私钥签名
-	)
-	if err != nil {
-		log.Fatalf("无法创建证书: %v", err)
-	}
-	// 4. 编码证书为 PEM 格式
-	certPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: certDER,
-	})
-	// 5. 编码私钥为 PEM 格式（PKCS#8）
-	privateKeyDER, err := x509.MarshalPKCS8PrivateKey(privateKey)
-	if err != nil {
-		log.Fatalf("无法编码私钥: %v", err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: privateKeyDER,
-	})
-	return certPEM, keyPEM
+	// 直接生成 DER 并编码为 PEM
+	der, _ := x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, &key.PublicKey, key)
+	c := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: der})
+
+	// 缩减私钥编码步骤
+	kDer, _ := x509.MarshalPKCS8PrivateKey(key)
+	k := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: kDer})
+
+	return c, k
 }()
